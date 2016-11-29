309a0fc417b34fe0923d4699383f27a22549d87c
Matthew Ouellette <matthew.paul.ouellette@gmail.com>
2016-11-28 22:46:11 -0400

    p5 - added routing for links and dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,15 @@
-import {Component} from "@angular/core";
-import {HeroesComponent} from './heroes.component'
+import {Component} from '@angular/core';
 
 @Component({
+  moduleId: module.id,
+  styleUrls: [ 'app.component.css' ],
   selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <my-heroes></my-heroes>
+  template: `<h1>{{title}}</h1>
+<nav>
+  <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+  <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+</nav>
+<router-outlet></router-outlet>
 `
 })
 
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,24 +1,30 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
+import {NgModule}       from '@angular/core';
+import {BrowserModule}  from '@angular/platform-browser';
+import {FormsModule}    from '@angular/forms';
+import {AppComponent}        from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent}     from './heroes.component';
+import {HeroService}         from './hero.service';
+import {DashboardComponent} from './dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
+
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   providers: [
     HeroService
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
